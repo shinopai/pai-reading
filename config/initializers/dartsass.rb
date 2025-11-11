29564ff5c0b03598024c83172eadc147b76a0ddc
shinopai <harbor.shinozaki@gmail.com>
2025-11-11 20:51:45 +0900

    ログイン＆新規登録フォーム作成

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,5 +1,6 @@
 Rails.application.config.dartsass.builds = {
   "application.scss" => "application.css",
   "styles/header.scss" => "styles/header.css",
-  "styles/book.scss" => "styles/book.css"
+  "styles/book.scss" => "styles/book.css",
+  "styles/auth.scss" => "styles/auth.css"
 }
